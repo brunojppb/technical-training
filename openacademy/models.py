8838499ee5aa8cde48468bd5b2218cf3b8c08b42
brunojppb <brunojppb@gmail.com>
2018-10-01 12:45:16 +0000

    onchange and constrains decorators

--- openacademy/models.py
+++ openacademy/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError, UserError
 
 class Courses(models.Model):
     _name = 'openacademy.course'
@@ -17,3 +18,25 @@ class Sessions(models.Model):
     start_date = fields.Date()
     seats = fields.Integer('Room Capacity')
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    
+    def _get_available_spots(self):
+        current = len(self.attendee_ids)
+        return self.seats - current
+    
+    # Constrains validations are triggered 
+    # all the time after the model changes
+    @api.constrains('attendee_ids')
+    def validate_attendees_constrains(self):
+        current = len(self.attendee_ids)
+        if self._get_available_spots() < 0:
+            raise ValidationError("CONSTRAIN")
+            
+    
+    # Onchange validations are only triggered when 
+    # changing the model via the user interface
+    @api.onchange('attendee_ids')
+    def validate_attendees_onchange(self):
+        if self._get_available_spots() < 0:
+            raise ValidationError("ONCHANGE")
+    
+    
